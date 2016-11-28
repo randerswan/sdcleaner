c6ac848dfeca770c3942e29fe1cb65d2a8be1a5c
liangxiwen86@gmail.com <liangxiwen86@gmail.com>
2016-11-29 00:04:12 +0800

    测试提交git

---
+++ sdcleaner/src/main/java/cn/liangxiwen/sdcleaner/SDCleanerMainActivity.java
@@ -0,0 +1,13 @@
+package cn.liangxiwen.sdcleaner;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SDCleanerMainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sdcleaner_main);
+    }
+}
